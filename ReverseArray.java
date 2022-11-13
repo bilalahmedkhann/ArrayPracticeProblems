efe56fdb5ef42e2bc7e27549dbf85a37cc8c9b12
Bilal Ahmed Khan <bilal.ahmed98@gmail.com>
2022-11-13 20:37:46 +0530

    ADDED:Write a program to reverse the array

---
+++ ReverseArray.java
@@ -0,0 +1,15 @@
+public class ReverseArray {
+    public static void main(String[] args) {
+        int []arr = {1,2,3,4,5,6,7,8,9};
+        int n=arr.length;
+        ReverseArray r = new ReverseArray();
+        r.reverse(arr,n);
+
+    }
+    public void reverse(int []arr, int n){
+        System.out.println("Reversed array is : ");
+        for(int i=n;i>=1;i--){
+            System.out.print(i+" ");
+        }
+    }
+}
